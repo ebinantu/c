0366830e68b547ab25a58c146e71379a7200953a
EBINANTU <152725871+ebinantu@users.noreply.github.com>
2025-01-03 14:50:38 +0530

    Update 10.c

--- 10.c
+++ 10.c
@@ -27,3 +27,37 @@ int main()
   }while(d=='y'||d=='Y');
     printf("%d postive numbers %d negative number %d zero",p,n,z);
 }
+
+
+
+
+<style>
+header {
+  width: 100%;
+  display: flex;
+  padding: 2px;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.links {
+  display: flex;
+  list-style: none;
+}
+
+.links li {
+    margin-left: 2px;
+    margin-right: 2px;
+}
+</style>
+<header>
+  <img width="100px" height="100px" src="https://picsum.photos/100/100" >
+
+  <ul class="links">
+    <li> Link1</li>
+    <li> Link2</li>
+    <li> Link3</li>
+    <li> Link4</li>
+  </ul>
+
+</header>
